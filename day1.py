7bdffe19d17a94cefa656f27fd6b2837841896b4
alvincodes-lgtm <alvinmunene474@gmail.com>
2026-07-16 22:38:29 +0300

    initial commit

---
+++ day1.py
@@ -0,0 +1,14 @@
+print("hello world")
+print("my name is alvin")
+print("hello world")
+print("good evening")
+print("what is your name")
+#py data types
+#123 ( integers)
+#Python is human readable language (sring)
+#3.12 -(float)
+#true -(boolean 1/2 true/false)
+# variable - containers for storing data
+age = 18
+print( "my name is alvin i am" , age , "years old")    
+print(f"my name is alvin iam {age}")
