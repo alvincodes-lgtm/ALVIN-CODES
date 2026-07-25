cae31bd668bc4b9a14cb46b47c732c00f70827da
alvincodes-lgtm <alvinmunene474@gmail.com>
2026-07-25 18:22:26 +0300

    ATM assignment

--- day06_assignment.py
+++ day06_assignment.py
@@ -3,12 +3,20 @@ name = input("enter your name ")
 password = input("password ")
 balance = input("enter your balance ")
 
-if name ==  "alvin" and password == "qwerty":
+if name ==  "alvin" and password == "qwerty" and balance == "10000":
     print("Welcome Back")
     print("balance: " + str(balance))
 
-elif name != "alvin" or password !="qwerty":
-    print("try again")
+elif name != "alvin":
+    print("unknown user")
+
+elif balance != "10000":
+    print("wrong balance")
+
+
+else:
+    print("incorrect password")
+
 
 
     
